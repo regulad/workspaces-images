5d6f8cd3c05d9ffa518763aee136262e3c141fa1
Parker Wahle <regulad@regulad.xyz>
2024-07-13 15:01:31 -0400

    sndcpy

---
+++ src/ubuntu/install/scrcpy/custom_sndcpy_startup.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+set -ex
+#START_COMMAND="/usr/bin/supervisord"
+DOCKER_PGREP="supervisord"
+SCRCPY_PGREP="scrcpy"
+SNDCPY_PGREP="vlc"
+#DEFAULT_ARGS="-n"
+#ARGS=${APP_ARGS:-$DEFAULT_ARGS}
+#ANDROID_VERSION=${ANDROID_VERSION:-"13.0.0"}
+#REDROID_GPU_GUEST_MODE=${REDROID_GPU_GUEST_MODE:-"guest"}
+SCRCPY_FPS=${SCRCPY_FPS:-"30"}
+SCRCPY_WIDTH=${SCRCPY_WIDTH:-"720"}
+SCRCPY_HEIGHT=${SCRCPY_HEIGHT:-"1280"}
+#REDROID_DPI=${REDROID_DPI:-"320"}
+SCRCPY_SHOW_CONSOLE=${SCRCPY_SHOW_CONSOLE:-"0"}
+REDROID_DISABLE_AUTOSTART=${REDROID_DISABLE_AUTOSTART:-"0"}
+REDROID_DISABLE_HOST_CHECKS=${REDROID_DISABLE_HOST_CHECKS:-"1"}
+ADB_DEVICE=${ADB_DEVICE:-"localhost:5555"}
+
+#ICON_ERROR="/usr/share/icons/ubuntu-mono-dark/status/22/system-devices-panel-alert.svg"
+
+
+#LAUNCH_CONFIG='/dockerstartup/redroid_launch_selections.json'
+## Launch Config Based Workflow
+#if [ -e ${LAUNCH_CONFIG} ]; then
+#  ANDROID_VERSION="$(jq -r '.android_version' ${LAUNCH_CONFIG})"
+#fi
+
+
+#function check_modules() {
+#  if lsmod | grep -q binder_linux; then
+#    echo "binder_linux module is loaded."
+#  else
+#      msg="Host level module binder_linux is not loaded. Cannot continue.\nSee https://github.com/remote-android/redroid-doc?tab=readme-ov-file#getting-started for more details."
+#      echo msg
+#      notify-send -u critical -t 0 -i "${ICON_ERROR}" "Redroid Error" "${msg}"
+#      exit 1
+#  fi
+#}
+
+function connect_adb() {
+  echo "Attempting ADB connection..."
+  adb connect "$ADB_DEVICE" | grep "connected"  # handles "connected to" and "already connected to"
+}
+
+start_android() {
+#  sleep 5
+#  xfce4-terminal --hide-menubar --command "bash -c \"sudo docker pull redroid/redroid:${ANDROID_VERSION}-latest \""
+#  sudo docker run -itd --rm --privileged \
+#    --pull always \
+#    -v ~/data:/data \
+#    -p 5555:5555 \
+#    redroid/redroid:${ANDROID_VERSION}-latest \
+#    androidboot.redroid_gpu_mode=${REDROID_GPU_GUEST_MODE} \
+#    androidboot.redroid_fps=${SCRCPY_FPS} \
+#    androidboot.redroid_width=${SCRCPY_WIDTH} \
+#    androidboot.redroid_height=${SCRCPY_HEIGHT} \
+#    androidboot.redroid_dpi=${REDROID_DPI}
+    echo "Connecting to ADB device..."
+    while ! connect_adb;
+    do
+      echo "Failed to connect to ADB device, retrying..."
+      sleep 10
+    done
+    echo "Connected to ADB device."
+}
+
+start_scrcpy() {
+  # https://github.com/Genymobile/scrcpy
+  # https://gist.github.com/regulad/047f1bbe20614681a263caaa16dee661
+#  window_x=$(($SCRCPY_WIDTH / 2))
+#  window_y=$(($SCRCPY_HEIGHT / 2))
+#  scrcpy_command="scrcpy -s \"$ADB_DEVICE\" --window-borderless --window-width=${SCRCPY_WIDTH} --window-height=${SCRCPY_HEIGHT} --window-x=${window_x} --window-y=${window_y} --shortcut-mod=lalt --max-fps=${SCRCPY_FPS}"
+  # https://github.com/rom1v/sndcpy
+  sndcpy_command="bash /opt/sndcpy/sndcpy \"${ADB_DEVICE}\""
+
+  export SNDCPY_APK=/opt/sndcpy/sndcpy.apk
+
+  start_android  # Ensure that the device is connected, reconnect if necessary
+
+  if [ "$SCRCPY_SHOW_CONSOLE" == "1" ]; then
+#    xfce4-terminal --hide-menubar --command "bash -c \"$scrcpy_command\"" &
+    xfce4-terminal --hide-menubar --command "bash -c \"$sndcpy_command\"" &
+  else
+#    eval "$scrcpy_command"
+    eval "$sndcpy_command"
+  fi
+
+#  wait_for_process $SCRCPY_PGREP
+  wait_for_process $SNDCPY_PGREP
+}
+
+wait_for_process() {
+  process_match=$1
+  timeout=60
+  interval=1
+  elapsed_time=0
+
+  echo "Waiting for $process_match..."
+  while [[ $elapsed_time -lt $timeout ]]; do
+      if pgrep -x $process_match > /dev/null; then
+          echo "$process_match is running, continuing..."
+          break
+      fi
+      sleep $interval
+      elapsed_time=$((elapsed_time + interval))
+  done
+
+  if ! pgrep -x $process_match > /dev/null
+  then
+      echo "Did not find process $process_match"
+  fi
+
+}
+
+kasm_startup() {
+    if [ -n "$KASM_URL" ] ; then
+        URL=$KASM_URL
+    elif [ -z "$URL" ] ; then
+        URL=$LAUNCH_URL
+    fi
+
+    if  [ -z "$DISABLE_CUSTOM_STARTUP" ] ||  [ -n "$FORCE" ]  ; then
+
+        echo "Entering process startup loop"
+        set +x
+        while true
+        do
+            if ! pgrep -x $DOCKER_PGREP > /dev/null
+            then
+                set +e
+                sudo /usr/bin/supervisord -n &
+                set -e
+                if [ "$REDROID_DISABLE_AUTOSTART" == "0" ]; then
+                  start_android
+                fi
+            fi
+            if [ "$REDROID_DISABLE_AUTOSTART" == "0" ]; then
+                  if ! pgrep -x $SNDCPY_PGREP > /dev/null
+                  then
+                      start_scrcpy
+                  fi
+            fi
+            sleep 1
+        done
+        set -x
+
+    fi
+
+}
+
+/usr/bin/filter_ready
+/usr/bin/desktop_ready
+
+
+if [ "$REDROID_DISABLE_HOST_CHECKS" == "0" ]; then
+  check_modules
+fi
+kasm_startup
---
+++ src/ubuntu/install/scrcpy/install_sndcpy.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+set -ex
+
+ARCH=$(arch  | sed 's/x86_64/amd64/g')
+
+apt-get update
+apt-get install -y  android-tools-adb android-tools-fastboot \
+                    ffmpeg libsdl2-2.0-0 adb wget \
+                    gcc git pkg-config meson ninja-build libsdl2-dev \
+                    libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
+                    libswresample-dev libusb-1.0-0 libusb-1.0-0-dev jq gettext-base \
+                    xfce4-clipman xfce4-clipman-plugin unzip coreutils vlc
+
+mkdir -p /opt/
+cd /opt/
+
+## https://github.com/regulad/doppelganger-front/blob/7703ec1e640ac09ab81a53ecbbe6a4d0f00a7c59/scripts/download-scrcpy-server.sh
+## Set variables
+#DOWNLOAD_URL="https://nightly.link/regulad/scrcpy/workflows/scrcpy-server/feature%2Fwebsocket-v1.19.x/scrcpy-server.zip"
+#TEMP_DIR=$(mktemp -d)
+#DESTINATION_DIR="/opt/scrcpy-server"
+#DESTINATION_FILE="${DESTINATION_DIR}/scrcpy-server"
+#
+## Create destination directory if it doesn't exist
+#mkdir -p "$DESTINATION_DIR"
+#
+## Download the zip file
+#echo "Downloading scrcpy-server.zip..."
+#if ! curl -L "$DOWNLOAD_URL" -o "$TEMP_DIR/scrcpy-server.zip"; then
+#    echo "Failed to download the file. Please check your internet connection and try again."
+#    rm -rf "$TEMP_DIR"
+#    exit 1
+#fi
+#
+## Unzip the file
+#echo "Extracting scrcpy-server.zip..."
+#if ! unzip -q "$TEMP_DIR/scrcpy-server.zip" -d "$TEMP_DIR"; then
+#    echo "Failed to extract the zip file. It might be corrupted."
+#    rm -rf "$TEMP_DIR"
+#    exit 1
+#fi
+#
+## Copy and rename the server file
+#echo "Copying server file to $DESTINATION_FILE..."
+#if ! cp "$TEMP_DIR/scrcpy-server" "$DESTINATION_FILE"; then
+#    echo "Failed to copy the server file to the destination."
+#    rm -rf "$TEMP_DIR"
+#    exit 1
+#fi
+#
+## Clean up
+#rm -rf "$TEMP_DIR"
+#
+#echo "Successfully downloaded and placed scrcpy-server.jar in $DESTINATION_FILE"
+#
+#git clone https://github.com/regulad/scrcpy
+#cd scrcpy
+#git checkout feature/websocket-v1.19.x
+#
+#BUILD_DIR=/opt/scrcpy/build-auto
+#
+#meson "$BUILD_DIR" --buildtype release --strip -Db_lto=true -Dprebuilt_server=$DESTINATION_FILE
+#ninja -C "$BUILD_DIR"
+#ninja -C "$BUILD_DIR" install
+
+# now we need to install sndcpy as well
+cd /opt/
+
+# Define the URL and destination directory with SNDCPY_ prefix for environment variables
+SNDCPY_DOWNLOAD_URL="https://github.com/rom1v/sndcpy/releases/download/v1.1/sndcpy-v1.1.zip"
+SNDCPY_DESTINATION_DIR="/opt/sndcpy"
+SNDCPY_TEMP_DIR=$(mktemp -d)
+
+# Ensure the destination directory exists
+mkdir -p "$SNDCPY_DESTINATION_DIR"
+
+# Download the zip file to the temporary directory
+echo "Downloading sndcpy.zip..."
+curl -L "$SNDCPY_DOWNLOAD_URL" -o "$SNDCPY_TEMP_DIR/sndcpy.zip"
+
+# Unzip the file to the destination directory
+echo "Extracting sndcpy.zip to $SNDCPY_DESTINATION_DIR..."
+unzip -q "$SNDCPY_TEMP_DIR/sndcpy.zip" -d "$SNDCPY_DESTINATION_DIR"
+
+# Clean up the temporary directory
+rm -rf "$SNDCPY_TEMP_DIR"
+
+echo "sndcpy has been successfully downloaded and extracted to $SNDCPY_DESTINATION_DIR"
