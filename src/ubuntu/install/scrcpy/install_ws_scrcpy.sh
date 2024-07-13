443f2482b323ae8b7acfbcc378ff157138b60e12
Parker Wahle <regulad@regulad.xyz>
2024-07-13 11:08:24 -0400

    version built against ws-scrcpy

---
+++ src/ubuntu/install/scrcpy/install_ws_scrcpy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+ARCH=$(arch  | sed 's/x86_64/amd64/g')
+
+apt-get update
+apt-get install -y  android-tools-adb android-tools-fastboot \
+                    ffmpeg libsdl2-2.0-0 adb wget \
+                    gcc git pkg-config meson ninja-build libsdl2-dev \
+                    libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
+                    libswresample-dev libusb-1.0-0 libusb-1.0-0-dev jq gettext-base \
+                    xfce4-clipman xfce4-clipman-plugin
+
+
+mkdir -p /opt/
+cd /opt/
+git clone https://github.com/Genymobile/scrcpy
+cd scrcpy
+./install_release.sh
