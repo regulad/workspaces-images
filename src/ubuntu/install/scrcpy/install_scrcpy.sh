13d2bb42eabd4fc06d3db0b75f075d07fc202742
Parker Wahle <regulad@regulad.xyz>
2024-07-13 11:09:02 -0400

    swap scrcpy and ws-scrcpy

--- src/ubuntu/install/scrcpy/install_scrcpy.sh
+++ src/ubuntu/install/scrcpy/install_scrcpy.sh
@@ -9,57 +9,11 @@ apt-get install -y  android-tools-adb android-tools-fastboot \
                     gcc git pkg-config meson ninja-build libsdl2-dev \
                     libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
                     libswresample-dev libusb-1.0-0 libusb-1.0-0-dev jq gettext-base \
-                    xfce4-clipman xfce4-clipman-plugin unzip coreutils
+                    xfce4-clipman xfce4-clipman-plugin
+
 
 mkdir -p /opt/
 cd /opt/
-
-# https://github.com/regulad/doppelganger-front/blob/7703ec1e640ac09ab81a53ecbbe6a4d0f00a7c59/scripts/download-scrcpy-server.sh
-# Set variables
-DOWNLOAD_URL="https://nightly.link/regulad/scrcpy/workflows/scrcpy-server/feature%2Fwebsocket-v1.19.x/scrcpy-server.zip"
-TEMP_DIR=$(mktemp -d)
-DESTINATION_DIR="/opt/scrcpy-server"
-DESTINATION_FILE="${DESTINATION_DIR}/scrcpy-server"
-
-# Create destination directory if it doesn't exist
-mkdir -p "$DESTINATION_DIR"
-
-# Download the zip file
-echo "Downloading scrcpy-server.zip..."
-if ! curl -L "$DOWNLOAD_URL" -o "$TEMP_DIR/scrcpy-server.zip"; then
-    echo "Failed to download the file. Please check your internet connection and try again."
-    rm -rf "$TEMP_DIR"
-    exit 1
-fi
-
-# Unzip the file
-echo "Extracting scrcpy-server.zip..."
-if ! unzip -q "$TEMP_DIR/scrcpy-server.zip" -d "$TEMP_DIR"; then
-    echo "Failed to extract the zip file. It might be corrupted."
-    rm -rf "$TEMP_DIR"
-    exit 1
-fi
-
-# Copy and rename the server file
-echo "Copying server file to $DESTINATION_FILE..."
-if ! cp "$TEMP_DIR/scrcpy-server" "$DESTINATION_FILE"; then
-    echo "Failed to copy the server file to the destination."
-    rm -rf "$TEMP_DIR"
-    exit 1
-fi
-
-# Clean up
-rm -rf "$TEMP_DIR"
-
-echo "Successfully downloaded and placed scrcpy-server.jar in $DESTINATION_FILE"
-
-
-git clone https://github.com/regulad/scrcpy
+git clone https://github.com/Genymobile/scrcpy
 cd scrcpy
-git checkout feature/websocket-v1.19.x
-
-BUILDDIR=/opt/scrcpy/build
-
-meson "$BUILDDIR" --buildtype release --strip -Db_lto=true -Dprebuilt_server=$DESTINATION_FILE
-ninja -C "$BUILDDIR"
-ninja -C "$BUILDDIR" install
+./install_release.sh
